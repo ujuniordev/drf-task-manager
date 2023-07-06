74c4589c90fde98595c5f5d34dc85f9d8aa888d6
ujuniordev <ujunior@outlook.com>
2023-07-06 22:22:19 +0000

    Adjust filtering to prevent unauthorized access by adding a custom permission class

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import TodoSerializer
 from .models import Todo
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow GET, HEAD, OPTIONS requests for all users
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Allow PUT, PATCH, DELETE requests only if the user is the owner of the object
+        return obj.task_owner == request.user
 
 
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
-
-    def get_queryset(self):
-        user = self.request.user
-        return Todo.objects.filter(task_owner=user)
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
