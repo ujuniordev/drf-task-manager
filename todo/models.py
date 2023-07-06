4486485ba4183f64a48277662f207864ab68ae3c
ujuniordev <ujunior@outlook.com>
2023-07-06 22:50:28 +0000

    Adjusd owner field to Todo class

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Todo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
--- todo/views.py
+++ todo/views.py
@@ -5,16 +5,17 @@ from .models import Todo
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Allow GET, HEAD, OPTIONS requests for all users
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
-        # Allow PUT, PATCH, DELETE requests only if the user is the owner of the object
-        return obj.owner == request.user
+        return request.user.is_authenticated
 
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
 
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
