2aba4f19dc607bc7d1a75083d1c0c36a4f5878ef
ujuniordev <ujunior@outlook.com>
2023-09-14 10:15:37 +0000

    Remove comments

--- profiles/views.py
+++ profiles/views.py
@@ -11,29 +11,3 @@ from rest_framework.decorators import action
 class ProfileView(viewsets.ModelViewSet):    
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
-    #permission_classes = [IsOwnerOrReadOnly]
-
-    #def get_object(self, pk):
-    #    try:
-    #        profile = Profile.objects.get(pk=pk)
-    #        self.check_object_permissions(self.request, profile)
-    #        return profile
-    #    except Profile.DoesNotExist:
-    #        raise Http404
-
-    #def get(self, request, pk):
-    #    profile = self.get_object(pk)
-    #    serializer = ProfileSerializer(
-    #        profile, context={'request': request}
-    #    )
-    #    return Response(serializer.data)
-
-    #def put(self, request, pk):
-    #    profile = self.get_object(pk)
-    #    serializer = ProfileSerializer(
-    #        profile, data=request.data, context={'request': request}
-    #    )
-    #    if serializer.is_valid():
-    #        serializer.save()
-    #        return Response(serializer.data)
-    #    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
